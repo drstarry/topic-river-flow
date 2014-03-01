18f358b06b3ce03a554c5d316593ce252da73f77
Rui Dai <drstarry@gmail.com>
2014-02-28 21:42:59 -0800

    add fruchterman_reigold

--- force-directed layout/layout.py
+++ force-directed layout/layout.py
@@ -1,12 +1,23 @@
 from igraph import Graph,plot
-g = Graph([(0,1), (0,2), (2,3), (3,4), (4,2), (2,5), (5,0), (6,3), (5,6)])
+g = Graph([(0,1), (0,2), (0,3), (0,4), (5,2), (3,5), (5,0), (4,3), (3,6)],directed = True)
 g.vs["name"] = ["Alice", "Bob", "Claire", "Dennis", "Esther", "Frank", "George"]
+g.es["weight"] = [1,5,3,7,6,2,9,5,7]
 g.vs["age"] = [25, 31, 18, 47, 22, 23, 50]
 g.vs["gender"] = ["f", "m", "f", "m", "f", "m", "m"]
 g.es["is_formal"] = [False, False, True, True, True, False, True, False, False]
-layout = g.layout("grid_fr")
+layout = g.layout_fruchterman_reingold()
 g.vs["label"] = g.vs["name"]
 color_dict = {"m": "blue", "f": "pink"}
 g.vs["color"] = [color_dict[gender] for gender in g.vs["gender"]]
-# plot(g, layout = layout, bbox = (300, 300), margin = 20)
-print g.degree()
+# plot(g, layout = layout, bbox = (500, 500), margin = 20)
+visual_style = {}
+visual_style["vertex_size"] = 20
+visual_style["vertex_color"] = [color_dict[gender] for gender in g.vs["gender"]]
+visual_style["vertex_label"] = g.vs["name"]
+visual_style["edge_width"] = [1 + 2 * int(is_formal) for is_formal in g.es["is_formal"]]
+visual_style["layout"] = layout
+visual_style["bbox"] = (300, 300)
+visual_style["margin"] = 20
+# plot(g, **visual_style)
+
+print g
